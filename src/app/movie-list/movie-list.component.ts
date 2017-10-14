156eaa85ada1c62df18fcabb26bfd2c67a523986
josemal37 <josemal_37@yahoo.es>
2017-10-14 08:47:55 -0400

    adding an image icon for each movie

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -9,15 +9,18 @@ export class MovieListComponent implements OnInit {
   title = 'Popular Movies';
   movie1 = {
     title: 'Star Wars',
-    overview: 'Star Wars description...'
+    overview: 'Star Wars description...',
+    icon: 'http://cdn.images.express.co.uk/img/dynamic/galleries/64x64/246485.jpg'
   };
   movie2 = {
     title: 'IT',
-    overview: 'IT description...'
+    overview: 'IT description...',
+    icon: 'http://i.ebayimg.com/images/g/Xd8AAOSwlf5ZwsfS/s-l64.jpg'
   };
   movie3 = {
     title: 'Justice League',
-    overview: 'Justice League description...'
+    overview: 'Justice League description...',
+    icon: 'http://cdn.images.express.co.uk/img/dynamic/galleries/64x64/228887.jpg'
   };
   constructor() { }
 
