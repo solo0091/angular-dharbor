77089cc4d93b97b27374f54689fc34da53ecc4d0
MacBookAir <macbookair@MacBookAirs-MacBook-Air.local>
2017-10-13 21:14:49 -0400

    hola

---
+++ src/app/movie/movie.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MaterialModule} from '../material/material.module';
+import { MovieListComponent } from '../movie-list/movie-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+  ],
+  declarations: [MovieListComponent]
+})
+export class MovieModule { }
