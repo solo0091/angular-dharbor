4d95cad0e6d1852192f6ae02559c087428734fda
unknown <Chris@cbblpt012_1.sabsa.aero>
2017-10-14 10:54:39 -0400

    manejo del ngfor para control de arreglos

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie} from '../shared/model/movie';
+
 
 @Component({
   selector: 'adh-movie-list',
@@ -6,28 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-  title: string = 'helle world,hola  mundo'
+  title: string = 'helle world,hola  mundo';
 
-  movie1 = {
+  movies:Movie[]=[
+    {
+      title: 'Star Wars',
+      overview: 'description starwars',
+      pictureAvatar:'http://niceclipart.com/wp-content/uploads/2017/07/Darth-Vader-clipart-3.jpg'
+    }, {
+      title: 'IT',
+      overview: 'description it',
+      pictureAvatar:'http://wtfonline.mx/wp-content/uploads/2017/09/itatuendo.jpg'
+  
+  
+    }, {
+      title: 'liga de la justicia',
+      overview: 'description liga de la justicia',
+      pictureAvatar:'http://sm.ign.com/ign_es/screenshot/default/maxresdefault_efdj.jpg'
+    }
+
+  ];
+
+
+
+
+/* 
+  movie1:Movie = {
     title: 'Star Wars',
-    overview: 'description starwars'
+    overview: 'description starwars',
+    pictureAvatar:'http://niceclipart.com/wp-content/uploads/2017/07/Darth-Vader-clipart-3.jpg'
   };
-  movie2 = {
+  movie2:Movie = {
     title: 'IT',
-    overview: 'description it'
+    overview: 'description it',
+    pictureAvatar:'https://cdn.dribbble.com/users/588874/screenshots/2249528/dribbble_1x.png'
+
+
   };
 
-  movie3 = {
+  movie3:Movie = {
     title: 'liga de la justicia',
-    overview: 'description liga de la justicia'
+    overview: 'description liga de la justicia',
+    pictureAvatar:'https://cdn.dribbble.com/users/588874/screenshots/2249528/dribbble_1x.png'
   };
-
+ */
 
 
   constructor() { }
 
   ngOnInit() {
-      setTimeout(()=> {
+      /* setTimeout(()=> {
         this.movie1.title="Star Wars II",
         this.movie1.overview="Corre tras 2 segundos";        
       },  8000);
@@ -35,7 +65,7 @@ export class MovieListComponent implements OnInit {
       setTimeout(()=> {
         this.movie1.title="Star Wars III",
         this.movie1.overview="upps  me adelante en la pelicula";        
-      },  4000);
+      },  4000); */
 
   }
 
