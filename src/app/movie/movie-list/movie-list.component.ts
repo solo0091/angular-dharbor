b79c6f86937c827241264c0029ec227ac8605037
alonedemon <mirko.flores@dharbor.com>
2017-10-13 21:36:43 -0400

    Modified movie list and added img

--- src/app/movie/movie-list/movie-list.component.ts
+++ src/app/movie/movie-list/movie-list.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-
+  title:string="Movie List";
+  movie1={
+    title:'Star Wars',
+    overview:"Star Wars description"
+  }
+  movie2={
+    title:'Star Wars2',
+    overview:"Star Wars description2"
+  }
+  movie=[
+    {
+      title:'Star Wars',
+      overview:"Star Wars description"
+    },
+    {
+      title:'Star Wars2',
+      overview:"Star Wars description2"
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
   }
-
+  getTitle(){
+    return this.title;
+  }
 }
