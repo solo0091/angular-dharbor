b2b2374484c5210da27b65c4bde1d63cb927c529
luixaviles <luis.avilesz@gmail.com>
2017-10-14 10:54:18 -0400

    Using *ngFor for movies

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -8,21 +8,23 @@ import { Movie } from '../shared/model/movie';
 })
 export class MovieListComponent implements OnInit {
   title:string = 'Popular Movies';
-  movie1:Movie = {
-    title: 'Star Wars',
-    overview: 'Star Wars description...',
-    pictureURL: 'https://cdn.dribbble.com/users/588874/screenshots/2249528/dribbble_1x.png'
-  };
-  movie2:Movie = {
-    title: 'IT',
-    overview: 'IT description...',
-    pictureURL: 'https://i.ytimg.com/vi/QW7jpXrI_aY/maxresdefault.jpg'
-  };
-  movie3:Movie = {
-    title: 'Justice League',
-    overview: 'Justice League description',
-    pictureURL: 'https://img.cinemablend.com/quill/e/4/9/8/7/7/e4987751f9b900b2988cefcc196417bd6fab8468.jpg'
-  };
+  movies: Movie[] = [
+    {
+      title: 'Star Wars',
+      overview: 'Star Wars description...',
+      pictureURL: 'https://cdn.dribbble.com/users/588874/screenshots/2249528/dribbble_1x.png'
+    },
+    {
+      title: 'IT',
+      overview: 'IT description...',
+      pictureURL: 'https://i.ytimg.com/vi/QW7jpXrI_aY/maxresdefault.jpg'
+    },
+    {
+      title: 'Justice League',
+      overview: 'Justice League description',
+      pictureURL: 'https://img.cinemablend.com/quill/e/4/9/8/7/7/e4987751f9b900b2988cefcc196417bd6fab8468.jpg'
+    }
+  ];
 
   constructor() { }
 
