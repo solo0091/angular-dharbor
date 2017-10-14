a47f8136c6ece993a5114375ece26667e282f7e3
JVMTBB <josepe_789@hotmail.com>
2017-10-14 10:54:28 -0400

    Using ngFor to movie list

--- src/app/movie/movie-list/movie-list.component.ts
+++ src/app/movie/movie-list/movie-list.component.ts
@@ -9,24 +9,40 @@ import { Movie } from '../../shared/model/movie'
 export class MovieListComponent implements OnInit {
 
   title: string = 'Popular Movies';
-  movie1: Movie = {
-    title: 'Geostorm',
-    overview: 'Geostorm description..',
-    pictureURL: 'http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2017/08/geostorm_lead.jpg?itok=mCUhAmx8'
-  };
-  movie2: Movie = {
-    title: 'IT',
-    overview: 'IT description...',
-    pictureURL: 'http://s3.amazonaws.com/www.tomatazos.com/public/uploads/images/156296_1140x516.jpg'
-  };
-  movie3: Movie = {
-    title: 'Justice League',
-    overview: 'Justice League description...',
-    pictureURL: 'http://nerdist.com/wp-content/uploads/2017/03/justice-league-movie-team-photo-1200x630.jpg'
-  };
-  
+  movies: Array<Movie> = [
+    {
+      title: 'Geostorm',
+      overview: 'Geostorm description..',
+      pictureURL: 'http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2017/08/geostorm_lead.jpg?itok=mCUhAmx8'
+    },
+    {
+      title: 'IT',
+      overview: 'IT description...',
+      pictureURL: 'http://s3.amazonaws.com/www.tomatazos.com/public/uploads/images/156296_1140x516.jpg'
+    },
+    {
+      title: 'Justice League',
+      overview: 'Justice League description...',
+      pictureURL: 'http://nerdist.com/wp-content/uploads/2017/03/justice-league-movie-team-photo-1200x630.jpg'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.movies= [
+        {
+          title: 'La falla de San Andres',
+          overview: 'La falla de San Andres description..',
+          pictureURL: 'https://i.ytimg.com/vi/K8jAKIJL66c/hqdefault.jpg'
+        },
+        {
+          title: 'Batman el caballero de la noche',
+          overview: 'Batman el caballero de la noche description...',
+          pictureURL: 'https://www.cinepremiere.com.mx/imgsHistorico/u18/int_trailer_tdkr_.jpg'
+        }
+      ];
+    }, 4000);
   }
 }
