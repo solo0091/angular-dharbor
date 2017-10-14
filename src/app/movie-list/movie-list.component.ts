9e95bb67e8435081624783eeade2e1fd96e3aab6
tanis <tativalero@gmail.com>
2017-10-14 09:22:59 -0400

    Adicion del modulo movie y componente mat-list con mas ejemplos

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -6,10 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
+  title: string ='Popular Movies';
+  // text: string  ='Description text'
+  movie1 = {
+    title: 'Star Wars',
+    overview: 'Star Wars description...'
+  };
+
+  movie2 = {
+    title: 'IT',
+    overview: 'IT description...'
+  };
+
+  movie3 = {
+    title: 'Justice League',
+    overview: 'Justice League description...'
+  };
+
+  text= {
+    title_text : 'Title text',
+    description: 'Example description'
+  }
 
   constructor() { }
 
   ngOnInit() {
+    //setTimeout(function() {
+    setTimeout(() => {
+      this.movie1 ={
+        title: 'Start Wars II',
+        overview: 'Part II of the movie...'
+      }
+    }, 2000);
+
+    setTimeout(() => {
+      this.movie2 ={
+        title: 'Wonder Woman',
+        overview: 'Wonder Woman description'
+      }
+    }, 4000);
+
+    setTimeout(() => {
+      this.movie3 ={
+        title: 'BraveHeart',
+        overview: 'BraveHeart description'
+      }
+    }, 6000);
   }
+
+  // getTitle(): string{
+  //   return this.title;
+  // }
 
 }
