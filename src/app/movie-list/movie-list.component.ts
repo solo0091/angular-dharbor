96439cf66ba1b6120e2bb596d3a26d7643d59431
Rosa Arebalo <rousetemari@gmail.com>
2017-10-13 21:37:20 -0400

    lista con formato peliculas

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
+  title : string  = ' popular movies';
+  movie1={
+    title: 'Star war',
+    overview : 'star ward description'
+  };
+   movie2={
+     title : 'it',
+    overview : 'it description..'
+   };
+
+   movie3={
+     title : 'justic ede la liga',
+     overview : 'it description..'
+   };
 
   constructor() { }
 
   ngOnInit() {
   }
+  //getTitle : string(){
+  //  return title;
+  }
 
-}
+
