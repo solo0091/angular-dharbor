5566f30091109dd4e3e6057ded82e9f9aa2e55f3
L1zz1 <liz_zen11@hotmail.com>
2017-10-13 21:10:31 -0400

    Creacion Modulo movie y add movie-Lista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieListComponent } from 'app/movie-list/movie-list.component';
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: '',//root path
+    redirectTo: 'home',
+    pathMatch: 'full'
+
+  },
+  {
+    //redireccionamos q la lista sea la pagina home
+    path: 'home',
+    component: MovieListComponent
   }
 ];
 
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 import {
   MATERIAL_COMPATIBILITY_MODE,
   MatButtonModule,
   MatIconModule,
+  MatCheckboxModule,
+  MatListModule,
   MatToolbarModule
+  
 } from '@angular/material';
 
 @NgModule({
@@ -13,11 +17,16 @@ import {
     CommonModule,
     MatButtonModule,
     MatIconModule,
+    MatCheckboxModule,
+    MatListModule,
     MatToolbarModule
+    
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
+    MatCheckboxModule,
+    MatListModule,
     MatToolbarModule
   ],
   providers: [
