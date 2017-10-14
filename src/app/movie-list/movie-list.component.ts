45df6825ef72a2602bec94768e8fcec4a64eb08d
elianor <elianor@gmail.com>
2017-10-14 11:31:12 -0400

    Added to DI movies

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Movie} from '../shared/model/movie'
+import {MovieService} from '../shared/services/movie.service'
 
 @Component({
   selector: 'adh-movie-list',
@@ -11,49 +12,20 @@ export class MovieListComponent implements OnInit {
   title: string = 'Popular Movies'
   defaultPictureURL:string = "http://www.simplek12.com/wp-content/uploads/2015/06/movie_night.jpg"  
 
-   movies: Movie[]=[
-    {
-      title: 'Star Wars',
-      overview: 'Star Wars description',
-      pictureURL:'https://cdn.dribbble.com/users/588874/screenshots/2249528/dribbble_1x.png'
-    },
-    {
-      title: 'IT',
-      overview: 'IT description',
-      pictureURL:'http://www.panorama.com.ve/__export/1504655773452/sites/panorama/img/espectaculos/2017/09/05/it.jpg'
-    },
-    {
-      title: 'Justice League',
-      overview: 'Justice League description',
-      pictureURL:'http://cdn2-www.superherohype.com/assets/uploads/gallery/justice-league/17349762_1337611299666014_1321506846992253218_o.jpg'
-    },
-    {
-      title:"Minions",
-      overview:"Minions overview..",
-      pictureURL:""
-    }
-   ]
-   
-  movies2: Movie[]=[
-   {
-     title: 'cenicienta',
-     overview: 'cenicienta description',
-     pictureURL:'https://i.pinimg.com/736x/3c/65/84/3c6584d71efe40a2159689af9ca51e8d--disney-princess-cinderella-cinderella-birthday.jpg'
-   },
-   {
-     title: 'blanca nieves',
-     overview: 'blanca nieves description',
-     pictureURL:'https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-r1443i_baf1276f.jpeg?region=0,0,600,600&width=320'
-   }  ]
-
-  constructor() { }
- 
   
+  //DI
+  constructor(private movieService:MovieService)
+   {
+
+    }
+ 
+  movies: Movie[];
 
   ngOnInit() {
-    setTimeout(()=>{
-      this.movies =this.movies2
-      },2000);
+    // setTimeout(()=>{
+    //   this.movies =this.movies2
+    //   },2000);
+    this.movies = this.movieService.getMovies();
   }
  
   getTitle():string{
--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import {Movie} from '../model/movie'
 
 @Injectable()
 export class MovieService {
 
   constructor() { }
+
+  movies: Movie[]=[
+    {
+      title: 'Star Wars',
+      overview: 'Star Wars description',
+      pictureURL:'https://cdn.dribbble.com/users/588874/screenshots/2249528/dribbble_1x.png'
+    },
+    {
+      title: 'IT',
+      overview: 'IT description',
+      pictureURL:'http://www.panorama.com.ve/__export/1504655773452/sites/panorama/img/espectaculos/2017/09/05/it.jpg'
+    },
+    {
+      title: 'Justice League',
+      overview: 'Justice League description',
+      pictureURL:'http://cdn2-www.superherohype.com/assets/uploads/gallery/justice-league/17349762_1337611299666014_1321506846992253218_o.jpg'
+    },
+    {
+      title:"Minions",
+      overview:"Minions overview..",
+      pictureURL:""
+    }
+   ];
+   
+   getMovies():Movie[]{
+      return this.movies;
+   }
+  
 
 }
