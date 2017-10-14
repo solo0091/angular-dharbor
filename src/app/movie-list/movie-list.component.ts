4224a4c2ad736b164e8986fe0a06ec8efb2a0865
anghel7 <ghehahanghel@gmail.com>
2017-10-13 21:37:27 -0400

    using iterpolation in angular

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
 
+  title:string = 'Popular Movies';
+
+  movie1 = {
+    title: 'Star Wars',
+    overview: 'Star Wars description'
+  };
+  movie2 = {
+    title: 'IT',
+    overview: 'IT description'
+  };
+  movie3 = {
+    title: 'Justice League',
+    overview: 'Justice League description'
+  }
+
   constructor() { }
 
   ngOnInit() {
