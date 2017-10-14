26444c59ed1265ee85c6c571e57e80c4b050f3bc
alonedemon <mirko.flores@dharbor.com>
2017-10-13 21:08:53 -0400

    Added movie-list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { MovieListComponent } from './movie/movie-list/movie-list.component';
 const routes: Routes = [
-  {
+ /* {
     path: '',
     children: []
+  }*/
+  {
+    path:'home',
+    component:MovieListComponent
+  },
+  {
+    path:"",
+    redirectTo:'home',
+    pathMatch:'full'
   }
 ];
 
---
+++ src/app/movie/movie.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MaterialModule } from "../material/material.module";
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
+  declarations: [MovieListComponent]
+})
+export class MovieModule { }
