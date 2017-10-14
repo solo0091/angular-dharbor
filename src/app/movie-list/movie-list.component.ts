97445e8ccaafcceecb212af9612466bbdeb55917
ivanMise <ivanMise@gmail.com>
2017-10-13 21:35:58 -0400

    se adiciono mas funcionalidad e interpolacion

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -6,8 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-
+  title:string='popular movies';
+  movie1 ={
+    title:'Start Wars', overview: 'description'
+  };
+  movie2 ={
+    title:'It', overview: 'description'
+  };
+  movie3 ={
+    title:'Justice League', overview: 'description'
+  };
+  
+  
   constructor() { }
+  getTitle(){
+    return this.title;
+  }
 
   ngOnInit() {
   }
