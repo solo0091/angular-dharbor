0bee890cd9df66a9da7184f887df915c5aa6758c
f1c0 <butt_head31@hotmail.com>
2017-10-13 21:37:11 -0400

    Uso de Interpolacion en el texto

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
 
+  title:string='Popular Movies'; ///aca definimos la variable y esto se recuperara en el archivo .html
+
+  movie1={
+    title:'Thor',
+    overview:'Esperemos sea buena peli'
+  };
+  movie2={
+    title:'IT',
+    overview:'Da miedo'
+  };
+  movie3={
+    title:'Justice League',
+    overview:'El mejor es flash'
+  };
+
   constructor() { }
 
   ngOnInit() {
