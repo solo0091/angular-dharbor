d1b16bd300785af9f9fb0deb7af1d1ee44ba2510
unknown <Chris@cbblpt012_1.sabsa.aero>
2017-10-13 21:34:42 -0400

    nuevo cambio con la adicion de lista

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
+  title: string = 'Hola mundo'
+
+  movie1 = {
+    title: 'Star Wars',
+    overview: 'description starwars'
+  };
+  movie2 = {
+    title: 'IT',
+    overview: 'description it'
+  };
+
+  movie3 = {
+    title: 'liga de la justicia',
+    overview: 'description liga de la justicia'
+  };
+
+
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  getTitle(): string {
+    return this.title;
+  }
+
 
 }
