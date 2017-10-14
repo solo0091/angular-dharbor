eae1d20e9d57c6d87b518310abf426d5c70da3ad
elianor <elianor@gmail.com>
2017-10-14 10:46:33 -0400

    added sample to use a-ngfor

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -9,33 +9,29 @@ import {Movie} from '../shared/model/movie'
 export class MovieListComponent implements OnInit {
 
   title: string = 'Popular Movies'
- 
-    movie1: Movie = {
+    
+   movies: Movie[]=[
+    {
       title: 'Star Wars',
       overview: 'Star Wars description',
       pictureURL:'https://cdn.dribbble.com/users/588874/screenshots/2249528/dribbble_1x.png'
-    }
-  
-    movie2: Movie = {
+    },
+    {
       title: 'IT',
       overview: 'IT description',
       pictureURL:'http://www.panorama.com.ve/__export/1504655773452/sites/panorama/img/espectaculos/2017/09/05/it.jpg'
-    }
-  
-    movie3: Movie = {
+    },
+    {
       title: 'Justice League',
       overview: 'Justice League description',
       pictureURL:'http://cdn2-www.superherohype.com/assets/uploads/gallery/justice-league/17349762_1337611299666014_1321506846992253218_o.jpg'
     }
+   ]
   
   constructor() { }
  
   ngOnInit() {
-    setTimeout(()=>{
-    this.movie1 ={
-     title:'start wars II',overview:'part II of the movie',pictureURL:'https://cdn.dribbble.com/users/588874/screenshots/2249528/dribbble_1x.png'
-    }
-    },2000);
+
   }
  
   getTitle():string{
