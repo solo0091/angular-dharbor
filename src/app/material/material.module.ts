696e7190a9b80d483bacde15ec8ebc80f2c80a71
tanis <tativalero@gmail.com>
2017-10-13 21:11:08 -0400

    Adicion del modulo movie y componente mat-list

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// import { MatListModule } from '@angular/material';
 
 import {
   MATERIAL_COMPATIBILITY_MODE,
   MatButtonModule,
   MatIconModule,
+  MatListModule,
   MatToolbarModule
+  
 } from '@angular/material';
 
 @NgModule({
@@ -13,11 +16,14 @@ import {
     CommonModule,
     MatButtonModule,
     MatIconModule,
+    MatListModule,
     MatToolbarModule
+
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
+    MatListModule,
     MatToolbarModule
   ],
   providers: [
