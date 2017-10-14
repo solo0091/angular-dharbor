d5fbe427caa8e0c1ad55e5445846f36c3a7e79d7
Marcelo Aruzamen <aruzamen@gmail.com>
2017-10-13 20:52:28 -0400

    Added movie module Added movie-list component Added default redirect to 'home' route

---
+++ src/app/movie/movie.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MovieListComponent } from '../movie-list/movie-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MovieListComponent]
+})
+export class MovieModule { }
