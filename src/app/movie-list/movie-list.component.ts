aae1649c3d9b8546d35e5ffcb1bd644d8afff97b
Marcelo Aruzamen <aruzamen@gmail.com>
2017-10-14 11:11:16 -0400

    Added ngIf statement to display default avatars

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -9,6 +9,7 @@ import { Movie } from "../shared/model/movie";
 export class MovieListComponent implements OnInit {
 
   title: string = 'Popular Movies'
+  defaultPictureUrl = 'https://cdn2.iconfinder.com/data/icons/classic-development-circle/512/multimedia-512.png'
 
   movies: Movie[] = [
     {
@@ -23,13 +24,29 @@ export class MovieListComponent implements OnInit {
       title: 'Justice League',
       overview: 'Justice League description',
       pictureUrl: 'https://www.dekora.es/wp-content/uploads/2017/09/231088-DISCO-DE-AZUCAR-16-CM-SUPERMAN-Y-BATMAN-AZF_1-300x300.jpg'
+    },{
+      title: 'Minions',
+      overview: 'Minions description',
+      pictureUrl: null
     }
   ];
 
 constructor() { }
 
-ngOnInit() {
-}
+  ngOnInit() {
+    setTimeout(() => {
+      this.movies.push({
+          title: 'Avatar',
+          overview: 'Avatar description',
+          pictureUrl: 'http://i1.wp.com/www.rafaelvega.com/wp-content/uploads/Avatar-Movie-Soundtrack-2009-300x300.jpg?resize=300%2C300'
+        });
+      this.movies.push({
+        title: 'Impire of the Sun',
+        overview: 'Impire of the Sun description',
+        pictureUrl: 'http://4.bp.blogspot.com/-QA047u9woyQ/Vlg8gnzSvAI/AAAAAAAAOuI/XEj8fDafKdM/s1600/sol.jpg'
+      });
+    }, 3000);
+  }
 
 getTitle(): string {
   return this.title;
