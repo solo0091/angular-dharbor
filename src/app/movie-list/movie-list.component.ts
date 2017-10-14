de075a233e27c20d0a2c10836194fcc18e3273a1
josemal37 <josemal_37@yahoo.es>
2017-10-13 21:31:19 -0400

    add list of movies with title and overview using interpolation

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-
+  title = 'Popular Movies';
+  movie1 = {
+    title: 'Star Wars',
+    overview: 'Star Wars description...'
+  };
+  movie2 = {
+    title: 'IT',
+    overview: 'IT description...'
+  };
+  movie3 = {
+    title: 'Justice League',
+    overview: 'Justice League description...'
+  };
   constructor() { }
 
   ngOnInit() {
