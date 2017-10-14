0af40925ad552ae3f5b2f23576d152572bce1750
Cronoz1994 <mauriciovlnemesis@gmail.com>
2017-10-14 11:35:30 -0400

    Clase movie.service (Dependency injection)

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
-import { Movie } from '../shared/model/movie';
+import { Movie             } from '../shared/model/movie';
+import { MovieService      } from '../shared/services/movie.service';
 
 @Component({
   selector: 'adh-movie-list',
@@ -12,32 +13,36 @@ export class MovieListComponent implements OnInit {
   title: string = 'Popular Movies';
 
   defaultPictureURL: string = 'https://angular.io/assets/images/logos/angular/angular.svg';
+  movies: Movie[];
 
-  movies: Movie[] = [
-    {
-      title     : 'Star Wars',
-      overview  : 'Star Wars description..',
-      pictureURL: 'http://elordenmundial.com/wp-content/uploads/2017/01/starwarsinfive-1449855236991_large.jpg'
-    },
-    {
-      title     : 'IT',
-      overview  : 'It description..',
-      pictureURL: 'http://mouse.latercera.com/wp-content/uploads/2017/07/it.jpg'
-    },
-    {
-      title     : 'Injustic Leage',
-      overview  : 'Injustic Leage description..',
-      pictureURL: 'https://www.injustice.com/img/share.jpg'    
-    },
-    {
-      title     : 'Minions',
-      overview  : 'Minions description..',
-      pictureURL: null    
-    }
-  ];
+  // llevado a movie.service
+  // movies: Movie[] = [
+  //   {
+  //     title     : 'Star Wars',
+  //     overview  : 'Star Wars description..',
+  //     pictureURL: 'http://elordenmundial.com/wp-content/uploads/2017/01/starwarsinfive-1449855236991_large.jpg'
+  //   },
+  //   {
+  //     title     : 'IT',
+  //     overview  : 'It description..',
+  //     pictureURL: 'http://mouse.latercera.com/wp-content/uploads/2017/07/it.jpg'
+  //   },
+  //   {
+  //     title     : 'Injustic Leage',
+  //     overview  : 'Injustic Leage description..',
+  //     pictureURL: 'https://www.injustice.com/img/share.jpg'    
+  //   },
+  //   {
+  //     title     : 'Minions',
+  //     overview  : 'Minions description..',
+  //     pictureURL: null    
+  //   }
+  // ];
 
 
-  constructor() { }
+  constructor(private movieService: MovieService) {  // si es private le asigana ala variable correspondiente automaticamente
+    // this.movieService = movieService; ya no es necesario esto
+   }
 
   ngOnInit() {
     // setTimeout(() => {   // Notacion flecha para acceder al contexto externo
@@ -53,6 +58,9 @@ export class MovieListComponent implements OnInit {
     //     overview: 'Wonder Woman description..'
     //   }
     // }, 4000);
+    setTimeout(() => {
+      this.movies = this.movieService.getMovies();
+    }, 2000);
   }
 
   /*
--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -1,9 +1,37 @@
 
 import { Injectable } from '@angular/core';
+import { Movie      } from '../model/movie';
 
 @Injectable()
 export class MovieService {
 
+  movies: Movie[] = [
+    {
+      title     : 'Star Wars',
+      overview  : 'Star Wars description..',
+      pictureURL: 'http://elordenmundial.com/wp-content/uploads/2017/01/starwarsinfive-1449855236991_large.jpg'
+    },
+    {
+      title     : 'IT',
+      overview  : 'It description..',
+      pictureURL: 'http://mouse.latercera.com/wp-content/uploads/2017/07/it.jpg'
+    },
+    {
+      title     : 'Injustic Leage',
+      overview  : 'Injustic Leage description..',
+      pictureURL: 'https://www.injustice.com/img/share.jpg'    
+    },
+    {
+      title     : 'Minions',
+      overview  : 'Minions description..',
+      pictureURL: null    
+    }
+  ];
+
   constructor() { }
+
+  getMovies(): Movie[] {
+    return this.movies;
+  }
 
 }
