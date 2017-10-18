4b96f21bfb44b651fb4c5b1ec3b54d5639921fcd
luixaviles <luis.avilesz@gmail.com>
2017-10-18 18:48:24 -0400

    Adding Movie Detail behavior

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../shared/services/movie.service';
+import { Movie } from '../shared/model/movie';
 
 @Component({
   selector: 'adh-movie-detail',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-detail.component.css']
 })
 export class MovieDetailComponent implements OnInit {
+  movie: Movie;
 
-  constructor() { }
+  constructor(private movieService: MovieService,
+            private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+    .subscribe((params: any) => {
+      this.movieService.getMovie(params.id)
+      .subscribe(movie => this.movie = movie);
+    });
   }
-
 }
--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -4,6 +4,7 @@ import { Movie } from '../model/movie';
 import { Observable } from 'rxjs/Observable';
 
 const MOVIES_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc';
+const MOVIE_API:string = 'https://api.themoviedb.org/3/movie';
 const API_KEY = '0971ed323ba8081b990144eef9e02ace';
 const PICTURE_URL = 'https://image.tmdb.org/t/p/w160';
 @Injectable()
@@ -15,7 +16,11 @@ export class MovieService {
   constructor(private httpClient: HttpClient) { }
 
   getMovies(): Observable<any> {
-    return this.httpClient.get(`${MOVIES_API}&api_key=${API_KEY}`);
+    return this.httpClient.get<any>(`${MOVIES_API}&api_key=${API_KEY}`);
+  }
+
+  public getMovie(id: number): Observable<Movie> {
+    return this.httpClient.get<Movie>(`${MOVIE_API}/${id}?api_key=${API_KEY}`);
   }
 
   getMoviePicture(posterPath: string) {
