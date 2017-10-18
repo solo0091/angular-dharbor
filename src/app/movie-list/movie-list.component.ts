fd2e55006c2d31ca223c6addd27e0f100ac787d5
tanis <tativalero@gmail.com>
2017-10-17 20:26:29 -0400

    Cambios en movie services y ts

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -18,8 +18,20 @@ export class MovieListComponent implements OnInit {
 
   ngOnInit() {
     // setTimeout(()=>{
-      this.movies = this.movieService.getMovies();
+     // this.movies = this.movieService.getMovies();
     // }, 2000);
+    console.log('antes de llamar al servicio');
+    // Esta en un contexto mas interno
+    this.movieService.getMovies().then((result)=>{
+      console.log('Tengo resultado');
+      this.movies = result;
+    }).catch((error)=>{
+      console.log('error', error.message);
+      //this.message= error.message
+    });
+    console.log('Despues de movieService.getMovies()');
+    //Llamadas a otros servicios
+    //Otro tipo de procesamiento
     
   }
 
