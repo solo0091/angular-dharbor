38f3cbea33125349d79b883dcd35430e06388632
renet1988 <renetlgr@gmail.com>
2017-10-19 20:01:34 -0400

    Subscribe for params and getMovies

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { MovieService } from '../shared/services/movie.service'
+import { Movie } from '../shared/model/movie'
 
 @Component({
   selector: 'adh-movie-detail',
@@ -6,12 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-detail.component.css']
 })
 export class MovieDetailComponent implements OnInit {
-
-  constructor() { }
+  movie: Movie;
+  constructor(private activatedRoute: ActivatedRoute,
+              private movieService: MovieService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+    .subscribe(params => {
+      console.log('Params', params);
+      this.movieService.getMovie(params.id)
+      .subscribe((movie: Movie) => {
+        this.movie = movie;
+      });
+    });
   }
-
-
 
 }
--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -7,6 +7,7 @@ const MOVIES_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=populari
 const API_KEY = '0971ed323ba8081b990144eef9e02ace';
 const PICTURE_URL = 'https://image.tmdb.org/t/p/w160';
 const DEFAULT_PICTURE_URL = 'https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/033/thumb/egghead-angular-material-course-sq.png'
+const MOVIE_DETAIL = 'https://api.themoviedb.org/3/movie'
 
 @Injectable()
 export class MovieService {
@@ -14,28 +15,6 @@ export class MovieService {
 
   movies: Movie[]
 
-  /* = [
-    {
-      title: 'Star Wars',
-      overview: 'Star Wars description....',
-      pictureURL: 'http://comic-cons.xyz/wp-content/uploads/Star-Wars-avatar-icon-Darth-Vader.png'
-    },
-    {
-      title: 'IT',
-      overview: 'IT description...',
-      pictureURL:'http://www.sopitas.com/wp-content/uploads/2016/07/tim-curry-pennywise-it-pel%C3%ADcula-4-e1468431683235.png'
-    },
-    {
-      title: 'Justice League',
-      overview: 'Justice League description ...',
-      pictureURL: 'http://previouslyempire.com/wp-content/uploads/2017/10/Justice_League_-_Logo_Signo.png'
-    },
-    {
-      title: 'Iron Man',
-      overview: 'Iron Man description....',
-      pictureURL: null
-    }
-  ]*/
   constructor(private httpClient: HttpClient) { }
 
   getMovies(): Observable<any> {
@@ -52,9 +31,8 @@ export class MovieService {
   getDefaultPictureURL(): string {
     return DEFAULT_PICTURE_URL;
   }
-/*
-  getMovieId(): Observable<any> {
-    return this.httpClient.get(`id`);
+
+  getMovie(id: number): Observable<any> {
+    return this.httpClient.get(`${MOVIE_DETAIL}/${id}?api_key=${API_KEY}`);
   }
-*/
 }
