82d26a27f99e30e53c1bcc42e7908e97681c6712
Marcelo Aruzamen <aruzamen@gmail.com>
2017-10-19 20:04:37 -0400

    Added changes to display movie details

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { MovieService } from "../shared/services/movie.service";
+import { Movie } from "../shared/model/movie";
 
 @Component({
   selector: 'adh-movie-detail',
@@ -9,17 +10,16 @@ import { MovieService } from "../shared/services/movie.service";
 })
 export class MovieDetailComponent implements OnInit {
 
+  movie: Movie;
+
   constructor(private movieService: MovieService, 
               private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((data)=>{
-      console.log(data);
-      console.log(data['id']);
-      this.movieService.getMovie(data['id'])
-      .subscribe(info => {
+      this.movieService.getMovie(data['id']).subscribe((info: Movie) => {
         console.log(info);
-        //this.movie = data.results;
+        this.movie = info;
       })
     });
   }
