62571a592134c0e41b525f7fc065c3afb109c42e
luixaviles <luis.avilesz@gmail.com>
2017-10-19 20:00:02 -0400

    Subscribe for params and getMovie API

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../shared/services/movie.service';
+import { Movie } from '../shared/model/movie';
 
 @Component({
   selector: 'adh-movie-detail',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-detail.component.css']
 })
 export class MovieDetailComponent implements OnInit {
+  movie: Movie;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private movieService: MovieService) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+    .subscribe(params =>{
+      console.log('Params', params);
+      this.movieService.getMovie(params.id)
+      .subscribe((movie: Movie) => {
+        this.movie = movie;
+      });
+    });
   }
 
 }
+
+
+
--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -4,6 +4,7 @@ import { Movie } from '../model/movie';
 import { Observable } from 'rxjs/Observable';
 
 const MOVIES_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc';
+const MOVIE_DETAIL = 'https://api.themoviedb.org/3/movie';
 const API_KEY = '0971ed323ba8081b990144eef9e02ace';
 const PICTURE_URL = 'https://image.tmdb.org/t/p/w160'; 
 const DEFAULT_PICTURE_URL = 'https://angular.io/assets/images/logos/angular/angular.png';
@@ -18,6 +19,10 @@ export class MovieService {
 
   getMovies(): Observable<any> {
     return this.httpClient.get(`${MOVIES_API}&api_key=${API_KEY}`);
+  }
+
+  getMovie(id: number): Observable<any> {
+    return this.httpClient.get(`${MOVIE_DETAIL}/${id}?api_key=${API_KEY}`);
   }
 
   getPictureURL(posterPath: string): string {
